9759f2a11746f3307c1b5d3143d8ac5026bd85a8
Haris Ganibegovic <ganibegovic8@gmail.com>
2025-12-08 12:25:29 +0100

    fix CORS issue

--- backend/app.py
+++ backend/app.py
@@ -17,7 +17,7 @@ app = FastAPI(title="App Metrics API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://sepjfrontendm.vercel.app"],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
